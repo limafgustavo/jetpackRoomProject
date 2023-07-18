117c24b971e5754a6df5813d9ab1d8cfaa4a3177
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-18 16:39:47 -0300

    The logic from AppDatabase has been created.

--- app/src/main/java/com/gustavolima/roomproject/database/AppDatabase.kt
+++ app/src/main/java/com/gustavolima/roomproject/database/AppDatabase.kt
@@ -1,7 +1,33 @@
 package com.gustavolima.roomproject.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gustavolima.roomproject.database.daos.UserDao
+import com.gustavolima.roomproject.database.models.User
 
-@Database
-class AppDatabase {
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun UserDao(): UserDao
+    companion object {
+
+        private const val DATABASE_NAME: String = "user_db"
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java, DATABASE_NAME
+            ).build()
+    }
 }
