0e4bc7b6e142ea442fc9fe32c3e0ef567755413d
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-18 16:14:13 -0300

    The user Dao logic has been implemented.

--- app/src/main/java/com/gustavolima/roomproject/database/daos/UserDao.kt
+++ app/src/main/java/com/gustavolima/roomproject/database/daos/UserDao.kt
@@ -1,7 +1,16 @@
 package com.gustavolima.roomproject.database.daos
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.gustavolima.roomproject.database.models.User
 
 @Dao
 interface UserDao {
+    @Insert
+    suspend fun insert(user: User)
+
+    @Query("SELECT COUNT(uid) FROM user")
+    suspend fun getTotalItens():Long
+
 }
