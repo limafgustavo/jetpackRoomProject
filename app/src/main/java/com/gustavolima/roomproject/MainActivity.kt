6d508cd16a997723e1810bb2555daeaf86cd931c
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-24 22:32:26 -0300

    New function created to load total users but not implemented and created a function to open the new user activity.

--- app/src/main/java/com/gustavolima/roomproject/MainActivity.kt
+++ app/src/main/java/com/gustavolima/roomproject/MainActivity.kt
@@ -3,22 +3,46 @@ package com.gustavolima.roomproject
 import android.content.Intent
 
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Dao
+import com.gustavolima.roomproject.database.AppDatabase
+import com.gustavolima.roomproject.database.daos.UserDao
 import com.gustavolima.roomproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var database: AppDatabase
+    private lateinit var userDao: UserDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        this.database = AppDatabase.getInstance(this)
+        this.userDao = this.database.UserDao()
+
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        loadTotalUsers()
+       openNewUserActivity()
+    }
+
+    private fun openNewUserActivity() {
         binding.buttonNewUser.setOnClickListener {
             val intent = Intent(this, NewUserActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun loadTotalUsers() {
+
     }
 }
